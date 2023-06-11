ee8902d4c1417bb05e10f809e45347837b0504df
Walyn <walyncurtis@gmail.com>
2023-06-11 09:10:12 -0500

    Adds pause menu as prefab

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public bool allowPause = true;
+    public GameObject obj;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && allowPause)
+        {
+            if (obj.activeInHierarchy)
+            {
+                Unpause();
+            }
+            else Pause();
+        }
+    }
+
+    void Pause()
+    {
+        Time.timeScale = 0f;
+        Cursor.lockState = CursorLockMode.None;
+        obj.SetActive(true);
+    }
+
+    void Unpause()
+    {
+        Time.timeScale = 1f;
+        Cursor.lockState = CursorLockMode.Locked;
+        obj.SetActive(false);
+    }
+}
